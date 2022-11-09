5f0e76e51f549178f64f20b80f8c7c6faf60a904
DavidSanchez <davidsm93@correo.ugr.es>
2022-11-09 19:19:49 +0100

    Subiendo la clase principal y primer metodo - avanza #5 #7

---
+++ f1department/f1department.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import f1department_data_join as f1dj
+
+class F1Department:
+	f1_data = pd.DataFrame()
+
+	def __init__(self):
+		f1_data_object = f1dj.F1DepartmentDataJoin()
+		self.f1_data = f1_data_object.df_f1_data
+
+	def driver_most_wins(self):
+		return 0;
