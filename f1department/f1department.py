af62b95b5f4ee95b701b44f8d819b0ba6ab1417b
DavidSanchez <davidsm93@correo.ugr.es>
2022-11-23 01:55:27 +0100

    Añadiendo metodo piloto con mas poles - avanza #5 - closes #8

--- f1department/f1department.py
+++ f1department/f1department.py
@@ -28,7 +28,22 @@ class F1Department:
 		return result
 
 	def driver_most_poles(self):
-		return 0;
+		folder = './data/'
+		df_drivers = pd.read_csv(folder+'drivers.csv')
+
+		total_poles = []
+		for i in df_drivers.index:
+			total_poles.append(0)
+		df_drivers['total_poles'] = total_poles
+
+		for i in df_drivers.index:
+			poles = len(self.f1_data[(self.f1_data['driverId']==df_drivers['driverId'][i])&(self.f1_data['grid']==1)])
+			df_drivers.at[i, 'total_poles'] = poles
+			poles = 0
+
+		driver_most_poles = df_drivers.query('total_poles == total_poles.max()')
+		result = driver_most_poles.to_json(orient="records")
+		return result
 
 	def nationality_most_wins(self):
 		return 0;
